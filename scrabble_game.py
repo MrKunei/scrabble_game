653c9a3af4bede64c3102949529a2b1a5f544381
Vladimir <vladimir.kunei@gmail.com>
2022-09-09 21:13:27 +0300

    более корректная проверка слов и удаление букв из списков

--- scrabble_game.py
+++ scrabble_game.py
@@ -18,17 +18,17 @@ def create_list_letters():
 
 def create_lists_user(list_letters, index):
     list_user = random.sample(list_letters, index)
-    for letter in list_user:
-        list_letters.remove(letter)
     return list_user
 
 
 def check_word_in_list(word, list_user):
     counter = 0
+    list_letter = list_user[:]
     letters_word = list(word)
     for letter in letters_word:
-        if letter in list_user:
+        if letter in list_letter:
             counter +=1
+            list_letter.remove(letter)
     if counter == len(letters_word):
         return True
     else:
@@ -37,83 +37,97 @@ def check_word_in_list(word, list_user):
 
 def check_word_in_file(word):
     with open("russian_word.txt", encoding="utf-8") as file:
-        if word in file.read():
-            return True
-        else:
-            return False
+        for line in file:
+            if word == line.rstrip():
+                return True
 
 
 def update_lists(list_letters, list_user, word):
     for letter in list(word):
         list_user.remove(letter)
+
     add_letter = create_lists_user(list_letters, len(word)+1)
     list_user += add_letter
-    add_letter_str = ", ".join( add_letter)
+
+    for add in add_letter:
+        list_letters.remove(add)
+
+    add_letter_str = ", ".join(add_letter)
     print(f"Добавляю следующие буквы: {add_letter_str}")
 
 
 def check_points(word):
     points_letter = {0: 0, 1: 0, 2: 0, 3: 3, 4: 6, 5: 7, 6: 8, 7: 9}
-    return int(points_letter[len(word)])
+    return points_letter[len(word)]
 
 
-def check_answer(list_user, name_user, list_letters, points):
+def check_answer(list_user, name_user, list_letters, points_user):
     while True:
         print(f"\nХодит {name_user}:")
-        print(f"Ваши буквы: '{list_user}'")
+        list_user_str = ", ".join(list_user)
+        print(f"Ваши буквы: '{list_user_str}'")
 
         word = input(f"Ваше слово: ")
         if word == "stop":
             return word
             break
-        if check_word_in_list(word, list_user):
+
+        elif check_word_in_list(word, list_user):
             if check_word_in_file(word):
+                point = check_points(word)
                 print(f"Такое слово есть\n{name_user} "
-                      f"получает {check_points(word)}")
-                points += check_points(word)
+                      f"получает {point}")
                 update_lists(list_letters, list_user, word)
+                points_user += int(point)
                 break
             else:
                 print(f"Такого слова нет\n{name_user} не получает очков")
                 list_user += create_lists_user(list_letters, 1)
                 add_letter = create_lists_user(list_letters, 1)
+
                 print(f"Добавляю следующие буквы: {add_letter}")
                 break
         else:
             print("Некоторых букв у вас нет. Повторите ввод")
             continue
 
-def show_winer(points_user_1, points_user_2, user_1, user_2):
-    if points_user_1 > points_user_2:
-        print(f"Победил {user_1}")
-        print(f"Счёт {points_user_1} : {points_user_2}")
+def show_winer(points_1, points_2, user_1, user_2):#????
+    if points_1 > points_2:
+        print(f"Победитель {user_1}")
+        print(f"Счёт {points_1} : {points_2}")
     else:
-        print(f"Победил {user_2}")
-        print(f"Счёт {points_user_1} : {points_user_2}")
+        print(f"Победитель {user_2}")
+        print(f"Счёт {points_1} : {points_2}")
 
 
 def main():
     print(f"*"*50 + "\nПривет.\nМы начинаем играть в Scrabble\n" + "*"*50)
     letters = create_list_letters()
 
-    name_user_1 = input(f"\nКак зовут первого игрока? ")
-    name_user_2 = input("Как зовут второго игрока? ")
+    name_user_1 = input(f"\nКак зовут первого игрока? ").lower().title()
+    name_user_2 = input("Как зовут второго игрока? ").lower().title()
     print(f"{name_user_1} vs {name_user_2}\n(раздаю случайные буквы)")
 
     letter_1 = create_lists_user(letters, 7)
     letter_2 = create_lists_user(letters, 7)
+    str_user_1 = ", ".join(letter_1)
+    str_user_2 = ", ".join(letter_2)
 
-    print(f"{name_user_1} - буква: '{letter_1}'")
-    print(f"{name_user_2} - буква: '{letter_2}'")
+    print(f"{name_user_1} - буква: '{str_user_1}'")
+    print(f"{name_user_2} - буква: '{str_user_2}'")
 
     points_1 = 0
     points_2 = 0
 
     while True:
-        word_1 = check_answer(letter_1, name_user_1, letters,points_1)
-        word_2 = check_answer(letter_2, name_user_2, letters, points_2)
 
-        if word_1 or word_2 == "stop":
+        word_1 = check_answer(letter_1, name_user_1, letters, points_1)
+        if word_1 == "stop":
+            show_winer(points_1, points_2, name_user_1, name_user_2)
+            break
+        word_2 = check_answer(letter_2, name_user_2, letters, points_2)
+        if word_2 == "stop":
+            print("-" * 25 + "GAME OVER" + "-" * 25)
             show_winer(points_1, points_2, name_user_1, name_user_2)
             break
 
