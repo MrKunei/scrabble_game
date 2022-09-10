d52b69e9be0fb2d7bf4d4673ce8173fe79092adf
Vladimir <vladimir.kunei@gmail.com>
2022-09-10 18:25:32 +0300

    статистика через список обновлена функция изменения списков при правильном ответе: -добавлена проверка основного списка букв на количество оставшихся
    добавлена функция изменения списка при неправильно ответе
    
    добавлен отредактированный файл со словами

--- scrabble_game.py
+++ scrabble_game.py
@@ -6,7 +6,6 @@ dict_alfa = {"а": 8, "б": 2, "в": 4, "г": 2, "д": 4, "е": 8, "ё": 1, "ж"
              "ч": 1, "ш": 1, "щ": 1, "ъ": 1, "ы": 2, "ь": 2, "э": 1, "ю": 1,
              "я": 2}
 
-
 def create_list_letters():
     """
     Создание списка букв из словаря
@@ -21,7 +20,7 @@ def create_list_letters():
 
 def create_lists_user(list_letters, index):
     """
-    Создание рандомного списка букв игрока
+    Создание рандомного списка букв для игрока
     """
     list_user = random.sample(list_letters, index)
     return list_user
@@ -29,11 +28,10 @@ def create_lists_user(list_letters, index):
 
 def check_word_in_list(word, list_user):
     """
-    Проверка на наличие букв введенного слова в списке игрока
+    Проверка наличия букв введенного слова в списке игрока
     """
     counter = 0
-    list_letter = list_user[
-                  :]  # копия списка игрока для проверки повторяющихся букв
+    list_letter = list_user[:]  #копия списка для проверки повторяющихся букв
     letters_word = list(word)
     for letter in letters_word:
         if letter in list_letter:
@@ -49,33 +47,45 @@ def check_word_in_file(word):
     """
     Проверка слова в списке файла
     """
-    with open("russian_word.txt", encoding="utf-8") as file:
+    with open("ru_word.txt", encoding="utf-8") as file:
         for line in file:  # чтение файла по строкам
             if word == line.rstrip():  # сравнение слова со строкой файла
                 return True
 
 
-def update_lists(list_letters, list_user, word):
+def update_lists_true_answer(list_letters, list_user, word):
     """
-    Обновление списков при правильном слове
+    Обновление списков если слово есть
     """
     for letter in list(word):
         list_user.remove(letter)  # удаление букв из списка игрока
-
-    add_letter = create_lists_user(list_letters, len(word) + 1)
-    list_user += add_letter
+    # проверка хватает ли букв в списке для добавления
+    if len(list_letters) >= len(list(word)) + 1:
+        add_letter = create_lists_user(list_letters, len(word) + 1)
+        list_user += add_letter
+    else:
+        add_letter = create_lists_user(list_letters, len(list_letters))
+        list_user += add_letter
 
     for add in add_letter:
-        list_letters.remove(
-            add)  # удаление из общего списка букв добавленных игроку
+        list_letters.remove(add)  #удаление из общего списка добавленных букв
 
     add_letter_str = ", ".join(add_letter)
     print(f"Добавляю следующие буквы: {add_letter_str}")
 
+def update_lists_false_answer(list_letters, list_user):
+    """
+    Обновление списка если слова нет
+    """
+    add_letter = create_lists_user(list_letters, 1)
+    list_user += add_letter
+    list_letters.remove(add_letter[0])
+    print(f"Добавляю следующие буквы: '{add_letter[0]}'")
+
 
 def check_points(word):
     """
-    Проверка количества баллов за правильный ответ, согласно количеству букв в слове
+    Возврат баллов за правильный ответ, согласно количеству букв в слове
     """
     points_letter = {0: 0, 1: 0, 2: 0, 3: 3, 4: 6, 5: 7, 6: 8, 7: 9}
     point = points_letter[len(word)]
@@ -86,12 +96,12 @@ def show_winer(points_1, points_2, user_1, user_2):
     """
     Вывод информации о победителе
     """
-    if points_1 > points_2:
+    if sum(points_1) > sum(points_2):
         print(f"Победитель {user_1}")
-        print(f"Счёт {points_1} : {points_2}")
+        print(f"Счёт {sum(points_1)} : {sum(points_2)}")
     else:
         print(f"Победитель {user_2}")
-        print(f"Счёт {points_1} : {points_2}")
+        print(f"Счёт {sum(points_1)} : {sum(points_2)}")
 
 
 def check_answer(list_user, name_user, list_letters, points_user):
@@ -105,30 +115,30 @@ def check_answer(list_user, name_user, list_letters, points_user):
         word = input(f"Ваше слово: ").lower()
 
         if word == "stop":  # проверка ввода на "stop" для выхода
-            return word
             break
-
+        # проверки на правильность слова
         elif check_word_in_list(word, list_user):
             if check_word_in_file(word):
                 point = check_points(word)
                 print(f"Такое слово есть\n{name_user} "
                       f"получает {point}")
-                update_lists(list_letters, list_user, word)
-                print(f"Ваши очки: {points_user}")
+                update_lists_true_answer(list_letters, list_user, word)
+                points_user.append(point)
                 break
             else:
                 print(f"Такого слова нет\n{name_user} не получает очков")
-                list_user += create_lists_user(list_letters, 1)
-                add_letter = create_lists_user(list_letters, 1)
-
-                print(f"Добавляю следующие буквы: {add_letter}")
+                update_lists_false_answer(list_letters, list_user)
                 break
         else:
             print("Некоторых букв у вас нет. Повторите ввод")
             continue
+    return word
 
 
 def main():
+    points_1 = [] # списки для учета баллов
+    points_2 = []
+
     print(f"-" * 50 + "\nПривет.\nМы начинаем играть в Scrabble\n" + "-" * 50)
     letters_list = create_list_letters()
 
@@ -146,30 +156,26 @@ def main():
     print(f"{name_user_2} - буква: '{str_user_2}'")
 
     while True:
-        points_1 = 0
-        points_2 = 0
+        if len(letters_list) == 0:  # если закончились буквы в списке
+            print("Буквы закончились.")
+            break
         """
         Ход первого игрока
         """
         word_1 = check_answer(letter_1, name_user_1, letters_list, points_1)
         if word_1 == "stop":
-            show_winer(points_1, points_2, name_user_1, name_user_2)
-            print("-" * 25 + "GAME OVER" + "-" * 25)
             break
         """
         Ход второго игрока
         """
         word_2 = check_answer(letter_2, name_user_2, letters_list, points_2)
         if word_2 == "stop":
-            print("-" * 25 + "GAME OVER" + "-" * 25)
-            show_winer(points_1, points_2, name_user_1, name_user_2)
             break
 
-        elif len(letters_list) == 0:  # если закончились буквы в списке
-            print("Буквы закончились.")
-            print("-" * 25 + "GAME OVER" + "-" * 25)
-            show_winer(points_1, points_2, name_user_1, name_user_2)
-            break
+
+
+    print("-" * 25 + "GAME OVER" + "-" * 25)
+    show_winer(points_1, points_2, name_user_1, name_user_2)
 
 
 if __name__ == '__main__':
